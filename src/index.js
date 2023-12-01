506d44bb61e3150f801a9362e0fc580600f5d98a
SS-okamura <ss.okamura0509@gmail.com>
2023-12-01 10:19:27 +0900

    feat: 背景用のコンポーネントの追加

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Knight from "./Components/Knight";
+import Square from "./Components/Square";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Knight />
+    <Square black>
+      <Knight />
+    </Square>
   </React.StrictMode>
 );
 
